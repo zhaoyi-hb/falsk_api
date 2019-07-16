ee2aa3e4907438860ca5c23131f169fcb5cb31b2
zhaoyi-hb <zhaoyi-hb@gmail.com>
2019-07-16 15:30:21 +0800

    singe mongodb data api used by flask

---
+++ flask_.py
@@ -0,0 +1,30 @@
+from flask import Flask,Response,json
+from pymongo import MongoClient
+app=Flask(__name__)
+
+class MongoConn:
+    def __init__(self):
+        self.url='mongodb://wikipedia-user:ysukeg0704@139.129.208.69:40029/wikipedia'
+        self.client=self.get_client()
+
+    def get_client(self):
+        return MongoClient(self.url)
+
+@app.route('/')
+def get_one():
+    mongoconn = MongoConn()
+    db = mongoconn.client['wikipedia']
+    # col = db['wikipedia']
+    # x=col.find_one({},{"_id":0})
+    col1=db['Evidence-Based_Complementary_and_Alternative_Medicine']
+    y=col1.find({},{'_id':0}).sort('ts',-1).limit(1)
+    for i in y:
+        dd=i
+    return Response(json.dumps(dd,ensure_ascii=False))
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=6666)
+
+
+
+
